56b3196a9d19ec089581c998028d81fd970da8d3
Muh. Haydar M <m.haydar@agate.id>
2023-09-08 09:18:21 +0700

    dsada

---
+++ src/backendApi/api/about.ts
@@ -0,0 +1,15 @@
+export interface GetAllAboutResponseData {
+  guid: string;
+}
+
+export interface AboutData {
+  guid: string;
+  title: string;
+  description: string;
+  image: string;
+  hero: string;
+}
+
+export const postAbout = () => {};
+
+export const getAllAbout = () => {};
